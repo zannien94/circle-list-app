ebbdcd818124faf7190686f6568e8673181d8cc3
Zaneta <zannien94@gmail.com>
2022-10-02 17:47:52 +0200

    move getRandomColor to utils

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,7 @@ import { useState } from 'react'
 import { Stack } from '@mui/system'
 import Circle from './components/Circle'
 import Buttons from './components/Buttons'
-
-function getRandomColor() {
-  const letters = '0123456789ABCDEF'
-  let color = '#'
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-  return color
-}
+import { getRandomColor } from './utils/helpers'
 
 function App() {
   const [circleList, setCircleList] = useState<string[]>([])
