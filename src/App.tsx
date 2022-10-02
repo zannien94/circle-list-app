5e29790c1ff5847079b4d2f868afecf90e41e548
Zaneta <zannien94@gmail.com>
2022-10-02 16:58:32 +0200

    add mui buttons, craete circles handlers and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import './App.css'
 
 function App() {
+  const [circleList, setCircleList] = useState<string[]>([])
+
+  const handleAddCircle = (): void => {
+    setCircleList([...circleList, 'lorem ipsum'])
+  }
+
+  const handleRemoveLastCircle = (): void => {
+    const newCircleList = [...circleList]
+    newCircleList.pop()
+    setCircleList(newCircleList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ButtonGroup variant='outlined'>
+        <Button onClick={handleAddCircle}>Add Circle</Button>
+        <Button onClick={handleRemoveLastCircle}>Remove Last Circle</Button>
+      </ButtonGroup>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
